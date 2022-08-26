444f86e4ccabff41e7c0859f16612d3d13ec150e
Serhii Bilotserkivets <bilotserkivets.serhii@gmail.com>
2022-08-26 15:23:24 +0300

    Les_2

---
+++ Lessons_2_First_step.py
@@ -0,0 +1,17 @@
+print("Hello", "Beetroot!", sep=' ', end='\n'*2)
+print("Get start to do home work:", end='\n'*2)
+
+print('#'*9)
+for i in range(3):
+    print('#', end='\t'*2)
+    print('#')
+print('#'*9, end='\n'*2)
+
+for i in range(2):
+    print('#', end='\t'*2)
+    print('#')
+print('#'*9)
+
+for i in range(2):
+    print('#', end='\t'*2)
+    print('#')
