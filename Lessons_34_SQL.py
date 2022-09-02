20e68ccc24e51a4a35858ca3e8605392ec11888d
Serhii Bilotserkivets <bilotserkivets.serhii@gmail.com>
2022-09-02 23:27:17 +0300

    Lessons_34

--- Lessons_34_SQL.py
+++ Lessons_34_SQL.py
@@ -1,6 +1,7 @@
 from SQL_CON_QUERY import create_connection, execute_query, select_query
 from pprint import pprint
-
+#####################################################################
+#Task_1
 connection = create_connection('Les_34_task_1.sqlite')
 
 job_title_table = '''
@@ -79,3 +80,33 @@ execute_query(connection, update_workers)
 execute_query(connection, delete_workers)
 
 pprint(select_query(connection, select_workers))
+
+#####################################################################
+#Task_2
+connection = create_connection('hr.db')
+
+select_fullname = 'select first_name || " " || last_name from employees'
+for item in select_query(connection, select_fullname):
+    print(item)
+print('*'*80)
+select_department_id = 'select distinct department_id from employees'
+for item in select_query(connection, select_department_id):
+    print(item)
+print('*'*80)
+select_first_name = 'select * from employees ORDER BY first_name'
+for item in select_query(connection, select_first_name):
+    print(item)
+print('*'*80)
+select_fullname_salaryPF = \
+    'select first_name || " " || last_name, salary * 0.12 from employees'
+for item in select_query(connection, select_fullname_salaryPF):
+    print(item)
+print('*'*80)
+select_max_min_salary = 'select min(salary), max(salary) from employees'
+pprint(select_query(connection,select_max_min_salary))
+print('*'*80)
+select_month_salary = '' \
+                      'select first_name || " " || last_name,' \
+                      ' round(salary, 2) from employees'
+for item in select_query(connection, select_month_salary):
+    print(item)
